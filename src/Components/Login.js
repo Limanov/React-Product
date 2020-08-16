4b45de52ee1b531f6adffdd3c51e635ec8f01a9b
Limanov <kamilkalota@gmail.com>
2020-08-16 13:06:20 +0200

    Feat: Add Login page and css

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,8 +1,19 @@
 import * as React from "react";
-
+import "./Components.css"
 export const Login = ({ 
    }) => {
   return (
-    <div>Login</div>
+    <div className="login">
+        <div className="login-header">
+            <div>Logowanie</div>
+            <div className="login-close-btn">
+                <span className="x">+
+                </span>
+            </div>
+        </div>
+<div className="login-screen">
+bla bla bla
+</div>
+</div>
   );
 };
